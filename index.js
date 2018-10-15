c0cb94b012ae042e3f143fb612e1ea8934e1958d
athit <athits58@email.nu.ac.th>
2018-10-15 12:05:38 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.get('/', (req, res) => res.send('Hello World! Test'))
+
+app.listen(port, () => console.log('app listening on port'+port))
+
